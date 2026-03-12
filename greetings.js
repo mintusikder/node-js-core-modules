2f97f28e5b24d8514b19eb6e754f2264c25043b1
Mintu Sikder <mintusikder15@gmail.com>
2026-03-12 22:16:16 +0600

    added greeting

---
+++ greetings.js
@@ -0,0 +1,15 @@
+const args = process.argv;
+
+const name = args[2] || "World";
+const time = new Date().getHours();
+let greeting;
+
+if (time < 12) {
+  greeting = "Good Morning";
+} else if (time < 18) {
+  greeting = "Good Afternoon";
+} else {
+  greeting = "Good Evening";
+}
+
+console.log(`${greeting}, ${name}!`);
